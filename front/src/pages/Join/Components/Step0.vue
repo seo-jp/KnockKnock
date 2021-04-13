9c523b69fe0b3b0a9e504ebef79235bdd0e8476f
seo-jp <68701441+seo-jp@users.noreply.github.com>
2021-04-14 02:43:34 +0900

    Vue.js 프론트 작업 ( 로그인 ~ 회원가입 )

---
+++ front/src/pages/Join/Components/Step0.vue
@@ -0,0 +1,107 @@
+<template>
+    <div>
+
+        <div style="width:320px; margin:0 auto;">
+        <b-form class="mt-5" @submit="onSubmit" @reset="onReset" >
+
+            <h3 class="mb-2">유저 정보 작성</h3>
+            
+            <b-form-input
+            id="input-1"
+            class="mb-2"
+            v-model="form.name"
+            placeholder="이름"
+            type="name"
+            required
+            />
+
+            <b-form-input
+            id="input-2"
+            class="mb-2"
+            v-model="form.userId"
+            placeholder="사용자 ID"
+            type="text"
+            required
+            />
+
+            <b-form-input
+            id="input-3"
+            class="mb-2"
+            v-model="form.password"
+            placeholder="비밀번호"
+            type="password"
+            required
+            />
+
+            <b-form-input
+            id="input-4"
+            class="mb-2"
+            v-model="form.phone"
+            placeholder="연락처"
+            type="tel"
+            required
+            />
+
+            <b-form-input
+            id="input-5"
+            class="mb-3"
+            v-model="form.email"
+            placeholder="이메일"
+            type="email"
+            required
+            />
+
+            <Button text="입력완료" />
+
+        </b-form>
+
+        <BottomLine text="로그인 페이지로 돌아가기" url="/Login" />
+        </div>
+
+    </div>
+</template>
+
+<script>
+
+import Button from '../../../shared-components/small/Button.vue'
+import BottomLine from '../../../shared-components/small/BottomLine.vue'
+
+
+export default {
+    name: "Step0",
+    components: {
+        Button,
+        BottomLine,
+    },
+    data() {
+      return {
+        form: {
+          name: '',
+          userId: '',
+          password: '',
+          phone: '',
+          email: '',
+        },
+      }
+    },
+    methods: {
+      onSubmit(event) {
+        event.preventDefault()
+        alert(JSON.stringify(this.form))
+        this.$router.push('/JoinStep/1')
+      },
+      onReset(event) {
+        event.preventDefault()
+        this.form.name = ''
+        this.form.userId = ''
+        this.form.password = ''
+        this.form.phone = ''
+        this.form.email = ''
+      }
+    }
+}
+</script>
+
+<style>
+    @import '../../../assets/css/Login.css';
+</style>
---
+++ front/src/pages/Join/Components/Step4.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+      
+      <h3 class="mt-3 mb-5">회원가입을 축하합니다</h3>
+
+    </div>
+</template>
+
+<script>
+
+
+export default {
+    name: "Step4",
+    components: {
+    },
+    data() {
+      return {
+      }
+    },
+    methods: {
+    }
+}
+</script>
+
+<style>
+    @import '../../../assets/css/Login.css';
+</style>
